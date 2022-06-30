8fc07fba7782202ca6a76f9816faa970533bc1b5
sonuneha <107686831+sonuneha@users.noreply.github.com>
2022-06-30 14:42:34 +0530

    v1.2

--- Fibanacci.java
+++ Fibanacci.java
@@ -1,13 +1,9 @@
-class FibonacciExample1{  
-public static void main(String args[])  
-{    
- int n1=0,n2=1,n3,i,count=10;    
- System.out.print(n1+" "+n2);//printing 0 and 1    
-    
- for(i=2;i<count;++i)//loop starts from 2 because 0 and 1 are already printed    
- {    
-  n3=n1+n2;    
-  
- }    
-  
-}}  
+public class SumOfNumbers1  
+{  
+public static void main(String args[])   
+{  
+int n1 = 225, n2 = 115, sum;  
+sum = n1 + n2;  
+System.out.println("The sum of numbers is: "+sum);  
+}  
+}  
